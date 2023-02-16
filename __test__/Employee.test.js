d666a9740fd1a739d6a0813c264f64829c4b2dd2
Caliza <rances.rodriguez@gmail.com>
2023-02-16 10:28:31 -0800

    Edited code to unit test Employee.test.js

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -4,9 +4,12 @@ describe('Employee', () => {
     describe('Initialization', () => {
         it('should create an object with a name, id, email, and role if provided valid arguments', () => {
             const name ='Hommer';
+            const id = '';
+            const email = 'employee@email';
 
+            // const employee1 = new Employee(name);
 
-            expect(Employee.name).toEqual(name);
+            expect(employee1.name).toEqual(name);
 
 
         })
