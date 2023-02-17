e67e7bd11b2f9bfdbbb626fbc92ca376373b2903
Caliza <rances.rodriguez@gmail.com>
2023-02-17 09:54:41 -0800

    Added a .then() method for each employee class to index.js.

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 const employeeArray = [];
 
 const generateHTML = (employeeArr) =>
-    `<!DOCTYPE html>
+  `<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
@@ -15,8 +19,8 @@ const generateHTML = (employeeArr) =>
   <header class="p-5 mb-4 header bg-light">
     <div class="container">
     ${employeeArr.map(employee => {
-      if (employee.getRole() == 'Manager') {
-        return `
+    if (employee.getRole() == 'Manager') {
+      return `
         <h1 class="display-4">Hi! My name is ${employee.name}</h1>
       <p class="lead">I am from ${employee.getRole()}.</p>
       <h3>Example heading <span class="badge bg-secondary">Contact Me</span></h3>
@@ -25,9 +29,9 @@ const generateHTML = (employeeArr) =>
         <li class="list-group-item">Email: ${employee.email}</li>
       </ul>
         `
-      }
-      else if (employee.getRole() == 'Manager') {
-        return `
+    }
+    else if (employee.getRole() == 'Manager') {
+      return `
         <h1 class="display-4">Hi! My name is ${employee.name}</h1>
       <p class="lead">I am from ${employee.getRole()}.</p>
       <h3>Example heading <span class="badge bg-secondary">Contact Me</span></h3>
@@ -36,9 +40,9 @@ const generateHTML = (employeeArr) =>
         <li class="list-group-item">Email: ${employee.email}</li>
       </ul>
         `
-      }
-      else if (employee.getRole() == 'Manager') {
-        return `
+    }
+    else if (employee.getRole() == 'Manager') {
+      return `
         <h1 class="display-4">Hi! My name is ${employee.name}</h1>
       <p class="lead">I am from ${employee.getRole()}.</p>
       <h3>Example heading <span class="badge bg-secondary">Contact Me</span></h3>
@@ -47,8 +51,8 @@ const generateHTML = (employeeArr) =>
         <li class="list-group-item">Email: ${employee.email}</li>
       </ul>
         `
-      }
-    })}
+    }
+  })}
       
     </div>
   </header>
@@ -56,43 +60,72 @@ const generateHTML = (employeeArr) =>
 </html>`;
 
 // function name() {
-    
+
 
 inquirer
-    .prompt([
-        {
-            type: 'list',
-            name: 'name',
-            message: 'Team member name?',
-        },
-        {
-            type: 'input',
-            name: 'position',
-            message: 'Team member position?',
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: 'Team member id?',
-        },       
-        {
-            type: 'input',
-            name: 'github',
-            message: 'Enter GitHub Username',
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'Team member email?',
-        },
-    ])
-    .then((answers) => {
-        console.log(answers);
-        employeeArray.push(answers)
-        const htmlPageContent = generateHTML(employeeArray);
-      console.log(answers);
-        fs.writeFile('index.html', htmlPageContent, (err) =>
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Team member name?',
+    },
+    {
+      type: 'input',
+      name: 'position',
+      message: 'Team member position?',
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: 'Team member id?',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter GitHub Username',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Team member email?',
+    },
+  ])
+  .then((answers) => {
+    const manager = new Manager(
+      answers.name,
+      answers.id,
+      answers.email,
+      answers.github
+    ); fs.writeFile('index.html', htmlPageContent, (err) =>
       err ? console.log(err) : console.log('Successfully created index.html!')
     );
-  });
-``
+  })
+  .then((answers) => {
+    const engineer = new Engineer(
+      answers.name,
+      answers.id,
+      answers.email,
+      answers.github
+    ); fs.writeFile('index.html', htmlPageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created index.html!')
+    );
+  })
+  .then((answers) => {
+    const intern = new Intern(
+      answers.name,
+      answers.id,
+      answers.email,
+      answers.github
+    ); fs.writeFile('index.html', htmlPageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created index.html!')
+    );
+  })
+    // .then((answers) => {
+    //     console.log(answers);
+    //     employeeArray.push(answers)
+    //     const htmlPageContent = generateHTML(employeeArray);
+    //     console.log(answers);
+    //     fs.writeFile('index.html', htmlPageContent, (err) =>
+    //         err ? console.log(err) : console.log('Successfully created index.html!')
+    //     );
+    // });
